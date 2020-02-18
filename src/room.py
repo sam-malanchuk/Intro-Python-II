9bde9a10668963dbee4e55aad97b9124988a7170
unknown <malanchuksam@gmail.com>
2020-02-17 22:13:16 -0500

    create the Room Class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, name, action):
+        self.name = name
+        self.action = action
+    
+    def __str__(self):
+        output = f'You entered {self.name} and {self.action}'
+        return output
